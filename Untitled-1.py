681c42e3af54de38a9885a8d94735730e8a28ce9
Vivek Sonar <99745096+viveksonar23@users.noreply.github.com>
2025-02-19 00:50:46 -0500

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -5,8 +5,6 @@ import streamlit as st
 
 
 def load_llm():
-    DEEPSEEK_API_KEY = os.environ.get("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")
-    
     if not DEEPSEEK_API_KEY:
         st.error("DeepSeek API key is not set.")
         return None
