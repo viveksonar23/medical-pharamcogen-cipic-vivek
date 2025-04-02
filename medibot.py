3e76ca7d10f05d9b2c198df55a66ff80415e430e
Vivek Sonar <99745096+viveksonar23@users.noreply.github.com>
2025-04-02 12:00:11 -0400

    Update medibot.py

--- medibot.py
+++ medibot.py
@@ -257,20 +257,8 @@ def load_llm2():
 from langchain_huggingface import HuggingFaceEndpoint
 import streamlit as st
 
-# def load_llm():
-#     HUGGINGFACE_REPO_ID = "mistralai/Mistral-7B-Instruct-v0.3"
-#     HF_TOKEN = st.secrets["HF_TOKEN"]  # ✅ read token securely from secrets
-
-#     return HuggingFaceEndpoint(
-#         repo_id=HUGGINGFACE_REPO_ID,
-#         task="text-generation",  # ✅ required
-#         temperature=0.5,
-#         model_kwargs={"max_length": 1024},
-#         huggingfacehub_api_token=HF_TOKEN
-#     )
-
 def load_llm():
-    HUGGINGFACE_REPO_ID = "HuggingFaceH4/zephyr-7b-beta"
+    HUGGINGFACE_REPO_ID = "mistralai/Mistral-7B-Instruct-v0.3"
     HF_TOKEN = st.secrets["HF_TOKEN"]  # ✅ read token securely from secrets
 
     return HuggingFaceEndpoint(
@@ -280,6 +268,18 @@ def load_llm():
         model_kwargs={"max_length": 1024},
         huggingfacehub_api_token=HF_TOKEN
     )
+
+# def load_llm():
+#     HUGGINGFACE_REPO_ID = "HuggingFaceH4/zephyr-7b-beta"
+#     HF_TOKEN = st.secrets["HF_TOKEN"]  # ✅ read token securely from secrets
+
+#     return HuggingFaceEndpoint(
+#         repo_id=HUGGINGFACE_REPO_ID,
+#         task="text-generation",  # ✅ required
+#         temperature=0.5,
+#         model_kwargs={"max_length": 1024},
+#         huggingfacehub_api_token=HF_TOKEN
+#     )
 
 def load_llm11():
     from langchain.llms import OpenAI
